deecb5236dee519324146fb4e75dd81ad4e11976
Delta <dhriti.baruah20@gmail.com>
2022-11-04 11:39:50 +0530

    tailwind installed

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import React, { useState } from 'react';
-import Create from './component/Create';
 import Nav from './component/Nav';
 import Show from './component/Show';
 import Edit from './component/Edit';
 import Post from './component/Post';
+import Home from './component/Home';
 import Createquill from './component/Createquill';
 import { Routes, Route } from "react-router-dom";
 import { itemStateContext } from './component/Context'
@@ -17,7 +17,7 @@ function App() {
       <itemStateContext.Provider value={{ singleItem, setsingleItem }}>
         <Nav />
         <Routes>
-          <Route path="Create" element={<Create />} />
+          <Route path="/" element={<Home />} />
           <Route path="Createquill" element={<Createquill />} />
           <Route path="Show" element={<Show />} />
           <Route path="/edit" element={<Edit />} />
