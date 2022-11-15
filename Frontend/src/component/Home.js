69a805603d86116c901d57d9f7771cc637726911
Delta <dhriti.baruah20@gmail.com>
2022-11-15 13:35:23 +0530

    initial commit on home

--- Frontend/src/component/Home.js
+++ Frontend/src/component/Home.js
@@ -1,10 +1,48 @@
 import React from 'react'
+import image5 from './images/img5.jpg'
+import imagePremium from './images/imagePremium.jpg'
+import imageBudget from './images/imageBudget.png'
+import accessories from './images/imageAccessories.jpg'
+import image1 from './images/img1.jpg'
 
 const Home = () => {
     return (
         <>
            <section id="home">
-          
+            <div className='relative'>
+                <img src={image5} alt="" />
+                <div className='absolute text-white top-14 md:top-36 right-36 text-xl md:text-4xl'>
+                    <h1 className='italic font-serif'>Grand Festive Offers</h1>
+                    <hr />
+                    <p className='font-bold'>This Festive Season <br />
+                        <span className='text-orange-300'>Buy Any Laptop</span> <br />
+                        & Get Amazing Offers
+                    </p>
+                    <p className='text-lg'>Limited OFFERS!! Hurry Up</p>
+                </div>
+            </div>
+           </section>
+           <section id="slides" className='flex flex-row justify-evenly py-5 bg-black text-white font-bold'>
+            <div className='flex flex-col justify-center'>
+                <div><img src={imagePremium} alt="" className='w-56 h-56 object-contain'/></div>
+                <h3 className='text-lg text-center'>Premium Laptops</h3>
+                <button className='p-3 bg-red-600 hover:bg-orange-400 hover:cursor-pointer'>Shop Now</button>
+            </div>
+            <div className='flex flex-col justify-center'>
+                <div><img src={imageBudget} alt="" className='w-56 h-56 object-contain'/></div>
+                <h3 className='text-lg text-center'>Budget Laptops</h3>
+                <button className='p-3 bg-red-600 hover:bg-orange-400 hover:cursor-pointer'>Shop Now</button>
+            </div>
+            <div className='flex flex-col justify-center'>
+                <div><img src={image1} alt=""className='w-56 h-56 object-contain' /></div>
+                <h3 className='text-lg text-center'>New Arrivals</h3>
+                <button className='p-3 bg-red-600 hover:bg-orange-400 hover:cursor-pointer'>Shop Now</button>
+            </div>
+            <div className='flex flex-col justify-center'>
+                <div><img src={accessories} alt="" className='w-56 h-56 object-contain'/></div>
+                <h3 className='text-lg text-center'>Accessories</h3>
+                <button className='p-3 bg-red-600 hover:bg-orange-400 hover:cursor-pointer'>Shop Now</button>
+            </div>
            </section>
         </>
     )
--- Frontend/src/component/Nav.js
+++ Frontend/src/component/Nav.js
@@ -1,18 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 
 const Nav = () => {
+    const [visibility, setVisibility] = useState('hidden')
+
     return (
         <>
-             <nav className='flex flex-row justify-between bg-indigo-800 font-bold'>
-                <ul className=' flex flex-row text-white h-12 text-lg space-x-10'>
+             <nav className='flex flex-row justify-between bg-black font-bold'>
+                <i className="material-icons p-3 text-white hover:text-yellow-400 hover:cursor-pointer block md:hidden" onClick={()=>{setVisibility(!visibility)}}>reorder</i>
+                <ul className='hidden md:flex flex-row text-white h-12 text-lg space-x-10 '>
                     <li></li>
-                    <li className='hover:cursor-pointer hover:bg-white hover:text-orange-700 p-2'>Home</li>
-                    <li className='hover:cursor-pointer hover:bg-white hover:text-orange-700 p-2'>Shop</li>
-                    <li className='hover:cursor-pointer hover:bg-white hover:text-orange-700 p-2'>Products</li>
-                    <li className='hover:cursor-pointer hover:bg-white hover:text-orange-700 p-2'>Contact</li>
+                    <li className='hover:cursor-pointer hover:bg-white hover:text-orange-700 p-2'><Link to="/">Home</Link></li>
+                    <li className='hover:cursor-pointer hover:bg-white hover:text-orange-700 p-2'><Link to="shop">Premium Laptops</Link></li>
+                    <li className='hover:cursor-pointer hover:bg-white hover:text-orange-700 p-2'><Link to="products">Budget Laptops</Link></li>
+                    <li className='hover:cursor-pointer hover:bg-white hover:text-orange-700 p-2'><Link to="contact">Contact</Link></li>
                 </ul>
-                <ul className=' flex flex-row text-white h-12 text-lg space-x-3'>
+                <ul className=' flex flex-row text-white h-12 text-lg md:space-x-3 space-x-1'>
                     <li></li>
                     <li className='m-3'><input type="text" className='bg-white w-56 border-none px-2 font-thin italic' placeholder='Search items....'/></li>
                     <li><i className="material-icons hover:text-yellow-400 py-3 hover:cursor-pointer ">search</i></li>
@@ -22,6 +25,16 @@ const Nav = () => {
                     </li>
                 </ul>
             </nav>
+            
+            <div className={visibility?'hidden':null}>
+            <ul className='flex flex-col text-white p-2 text-lg h-[100vh] bg-black  absolute w-54 z-20'>
+                    <li></li>
+                    <li className='hover:cursor-pointer hover:bg-white hover:text-orange-700 p-1'><Link to="/">Home</Link></li>
+                    <li className='hover:cursor-pointer hover:bg-white hover:text-orange-700 p-1'><Link to="shop">Premium Laptops</Link></li>
+                    <li className='hover:cursor-pointer hover:bg-white hover:text-orange-700 p-1'><Link to="products">Budget Laptops</Link></li>
+                    <li className='hover:cursor-pointer hover:bg-white hover:text-orange-700 p-1'><Link to="contact">Contact</Link></li>
+                </ul>
+            </div> 
         </>
     )
 }
