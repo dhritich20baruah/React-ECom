44e6afbb02cbbaa5fd19424e3baf15103fbd42da
Delta <dhriti.baruah20@gmail.com>
2022-12-10 20:53:08 +0530

    pepperfry clone nav partially done

--- Frontend/src/component/Home.js
+++ Frontend/src/component/Home.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import image5 from './images/sofa.jpg'
-import imagePremium from './images/imagePremium.jpg'
-import imageBudget from './images/imageBudget.png'
-import accessories from './images/imageAccessories.jpg'
-import image1 from './images/img1.jpg'
-import ProductCard from './ProductCard'
+import deal7 from './images/deal7.jpg'
+import deal2 from './images/deal2.jpg'
+import deal3 from './images/deal3.jpg'
+
 
 const Home = () => {
     return (
@@ -12,42 +11,38 @@ const Home = () => {
            <section id="home">
             <div className='relative'>
                 <img src={image5} alt="" className='w-[100%]'/>
-                <div className='absolute text-white top-14 md:top-36 right-36 text-xl md:text-4xl'>
-                    <h1 className='italic font-serif'>Grand Festive Offers</h1>
+                <div className='absolute text-white top-14 md:top-96 bg-indigo-600 right-[50%] text-xl md:text-4xl'>
+                    <h1 className='italic font-serif'>Welcome to Maple</h1>
                     <hr />
                     <p className='font-bold'>This Festive Season <br />
-                        <span className='text-orange-300'>Buy Any Laptop</span> <br />
-                        & Get Amazing Offers
+                        <span className='text-orange-300'>Furnish your dream home.</span>
                     </p>
                     <p className='text-lg'>Limited OFFERS!! Hurry Up</p>
                 </div>
             </div>
            </section>
-           <section id="slides" className='flex flex-row justify-evenly py-5 bg-gradient-to-b from-[#292828] to-black text-white font-bold'>
-            <div className='flex flex-col justify-center'>
-                <div><img src={imagePremium} alt="" className='w-56 h-56 object-contain'/></div>
-                <h3 className='text-lg text-center'>Premium Laptops</h3>
-                <button className='p-3 bg-red-600 hover:bg-orange-400 hover:cursor-pointer'>Shop Now</button>
-            </div>
-            <div className='flex flex-col justify-center'>
-                <div><img src={imageBudget} alt="" className='w-56 h-56 object-contain'/></div>
-                <h3 className='text-lg text-center'>Budget Laptops</h3>
-                <button className='p-3 bg-red-600 hover:bg-orange-400 hover:cursor-pointer'>Shop Now</button>
-            </div>
-            <div className='flex flex-col justify-center'>
-                <div><img src={image1} alt=""className='w-56 h-56 object-contain' /></div>
-                <h3 className='text-lg text-center'>New Arrivals</h3>
-                <button className='p-3 bg-red-600 hover:bg-orange-400 hover:cursor-pointer'>Shop Now</button>
-            </div>
-            <div className='flex flex-col justify-center'>
-                <div><img src={accessories} alt="" className='w-56 h-56 object-contain'/></div>
-                <h3 className='text-lg text-center'>Accessories</h3>
-                <button className='p-3 bg-red-600 hover:bg-orange-400 hover:cursor-pointer'>Shop Now</button>
-            </div>
+           
+           <section id="services" className='flex flex-row justify-evenly text-orange-600 text-2xl font-bold m-10'>
+            <div><i className="material-icons m-4">airport_shuttle</i>Fast delivery</div>
+            <div><i className="material-icons m-4">call</i>24X7 Customer support</div>
+            <div><i className="material-icons m-4">credit_card</i>Secured payments</div>
            </section>
            
-           <section id="latestProduct">
-            <ProductCard/>
+           <section id="featuredProduct" className='m-10'>
+            <h1 className='text-center text-indigo-800 text-3xl font-mono font-bold'>FEATURED PRODUCTS</h1>
+            <hr />
+            <div className="flex flex-row">
+                <div className='bg-slate-300 w-[30%]'>
+                    <h3 className='italic text-xl text-indigo-600'>Pine wood sofa set</h3>
+                </div>
+                <img src={deal2} alt="" className='w-[30%]'/>
+            </div>
+            <div className="flex flex-row">
+                <img src={deal3} alt="" className='w-[30%]'/>
+            </div>
+            <div className="flex flex-row">
+                <img src={deal7} alt="" className='w-[30%]'/>
+            </div>
            </section>
         </>
     )
--- Frontend/src/component/Nav.js
+++ Frontend/src/component/Nav.js
@@ -1,44 +1,87 @@
 import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
+import logo from './images/pepperfryLogo.jpg'
 
 const Nav = () => {
     const [visibility, setVisibility] = useState('hidden')
 
     return (
         <>
-            <div>
-                <ul className=' flex flex-row text-indigo-700 justify-between h-12 text-lg md:space-x-3 space-x-1 mx-4'>
-                    
-                    <li className="text-3xl font-bold">Maple</li>
+            <div className='flex flex-row justify-between md:px-10 bg-gray-100 py-3'>
+                <ul className='md:flex flex-row space-x-7 text-lg hidden'>
+                    <li></li>
+                    <li>Sell on Pepperfry</li>
+                    <li>Become a Franchisee</li>
+                    <li>Buy in Bulk</li>
+                    <li>Find a Studio</li>
+                    <li>Find Design Inspiration</li>
+                </ul>
+                <div className='flex flex-row'>
+                    <i className="material-icons p-3 text-black hover:text-yellow-400 hover:cursor-pointer block md:hidden" onClick={()=>{setVisibility(!visibility)}}>reorder</i>
+                    <ul className='flex md:flex-row flex-col md:space-x-5 text-sm'>
+                        <li className='capitalize'>Enter Pincode</li>
+                        <li className='md:block hidden'><i className="material-icons text-gray-400 underline">create</i></li>
+                        <li className='text-red-400 underline'>Find Pepperfry Studio</li>
+                    </ul>
+                </div>
+                {/* SM SCREEN */}
+                <div className='md:hidden block'>
+                    <i className="material-icons p-1 hover:cursor-pointer ">search</i>
+                    <i className="material-icons p-1 hover:cursor-pointer ">person_outline</i>
+                    <i className="material-icons p-1 hover:cursor-pointer ">favorite_border</i>
+                    <i className="material-icons p-1 hover:cursor-pointer">shopping_cart</i>
+                </div>
+            </div>
+            {/* MD SCREEN */}
+            <div className='md:block hidden'>
+                <ul className=' flex flex-row text-gray-700 h-12 text-lg md:space-x-3 space-x-1 ml-20 my-5'>                   
+                    <li><img src={logo} alt="" className='w-40'/></li>
                     <li className=''>
-                        <input type="text" className='bg-white w-56 border-none px-2 font-thin italic' placeholder='Search items....'/><i className="material-icons hover:text-yellow-400 py-3 hover:cursor-pointer ">search</i></li>
+                        <input type="text" className='bg-gray-200 px-2 h-10 w-[60vw] border-none font-thin' placeholder='Your door to happiness opens with a search'/><i className="material-icons bg-gray-200 hover:cursor-pointer h-10 pt-1 px-2">search</i></li>
                     <li>
-                        <div>                            
-                            <i className="material-icons p-3 hover:text-yellow-400 hover:cursor-pointer ">account_circle</i>
-                            <i className="material-icons p-3 hover:text-yellow-400 hover:cursor-pointer">shopping_cart</i>
+                        <div className='flex flex-row'>
+                            <button className='font-bold text-sm border-black border-2 px-1 rounded-2xl my-2 mx-2'>HELP</button>                            
+                            <i className="material-icons p-3 hover:cursor-pointer ">person_outline</i>
+                            <i className="material-icons p-3 hover:cursor-pointer ">favorite_border</i>
+                            <i className="material-icons p-3 hover:cursor-pointer">shopping_cart</i>
                         </div>
                     </li>
                 </ul>
             </div>
-             <nav className='flex flex-row justify-center bg-indigo-700 font-bold'>
-                <i className="material-icons p-3 text-white hover:text-yellow-400 hover:cursor-pointer block md:hidden" onClick={()=>{setVisibility(!visibility)}}>reorder</i>
-                <ul className='hidden md:flex flex-row text-white h-12 text-lg space-x-10 '>
+            <hr />
+            {/* MAIN NAV */}
+             <nav className=''>                
+                <ul className='hidden md:flex flex-row text-black h-12 text-md font-semibold ml-14 space-x-6 '>
                     <li></li>
-                    <li className='hover:cursor-pointer hover:bg-white hover:text-indigo-700 p-2'><Link to="/">Home</Link></li>
-                    <li className='hover:cursor-pointer hover:bg-white hover:text-indigo-700 p-2'><Link to="about">About</Link></li>
-                    <li className='hover:cursor-pointer hover:bg-white hover:text-indigo-700 p-2'><Link to="shop">Shop</Link></li>
-                    <li className='hover:cursor-pointer hover:bg-white hover:text-indigo-700 p-2'><Link to="contact">Contact</Link></li>
+                    <li className='hover:cursor-pointer hover:border-b-4 hover:border-orange-600 p-2'><Link to="/">Furniture</Link></li>
+                    <li className='hover:cursor-pointer hover:border-b-4 hover:border-orange-600 p-2'><Link to="about">Home Decor</Link></li>
+                    <li className='hover:cursor-pointer hover:border-b-4 hover:border-orange-600 p-2'><Link to="shop">Lamp & Lighting</Link></li>
+                    <li className='hover:cursor-pointer hover:border-b-4 hover:border-orange-600 p-2'><Link to="contact">Kitchen & Dinning</Link></li>
+                    <li className='hover:cursor-pointer hover:border-b-4 hover:border-orange-600 p-2'><Link to="/">Furnishings</Link></li>
+                    <li className='hover:cursor-pointer hover:border-b-4 hover:border-orange-600 p-2'><Link to="about">Matresses</Link></li>
+                    <li className='hover:cursor-pointer hover:border-b-4 hover:border-orange-600 p-2'><Link to="shop">Appliances</Link></li>
+                    <li className='hover:cursor-pointer hover:border-b-4 hover:border-orange-600 p-2'><Link to="contact">Pets</Link></li>
+                    <li className='hover:cursor-pointer hover:border-b-4 hover:border-orange-600 p-2'><Link to="shop">Modular</Link></li>
+                    <li className='hover:cursor-pointer hover:border-b-4 hover:border-orange-600 p-2'><Link to="contact">Gift Cards</Link></li>
                 </ul>
             </nav>
-            
+            {/* SIDENAV */}
             <div className={visibility?'hidden':null}>
-            <ul className='flex flex-col text-white p-2 text-lg h-[100vh] bg-black  absolute w-54 z-20'>
-                    <li></li>
-                    <li className='hover:cursor-pointer hover:bg-white hover:text-orange-700 p-1'><Link to="/">Home</Link></li>
-                    <li className='hover:cursor-pointer hover:bg-white hover:text-orange-700 p-1'><Link to="about">About</Link></li>
-                    <li className='hover:cursor-pointer hover:bg-white hover:text-orange-700 p-1'><Link to="shop">Shop</Link></li>
-                    <li className='hover:cursor-pointer hover:bg-white hover:text-orange-700 p-1'><Link to="contact">Contact</Link></li>
-                </ul>
+            <div className='h-[100vh] bg-white absolute w-[100%] z-20 top-0'>
+                 <div className='flex flex-row align-middle'>
+                    <i className="material-icons basis-1/6" onClick={()=>{setVisibility(!visibility)}}>arrow_back</i>
+                    <div className='bg-orange-600 text-white w-10 h-10 rounded-full relative basis-1/6'><h2 className='centerDiv absolute'>AS</h2></div>
+                    <div className='basis-1/6'>
+                        <p>Hey<br/><span>My Account</span></p>
+                    </div>
+                    <div className='basis-1/6'>Wallet</div>
+                    <div className='basis-1/6'><i className="material-icons">home</i><br/>Home</div>
+                 </div>
+                 <div>
+                    <div></div>
+                    <div></div>
+                 </div>
+            </div>
             </div> 
         </>
     )
