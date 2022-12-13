466b507235388298d33ca21670a1ae744904068a
Delta <dhriti.baruah20@gmail.com>
2022-12-13 15:19:08 +0530

    data created

--- Frontend/src/component/data.js
+++ Frontend/src/component/data.js
@@ -1,13 +1,199 @@
 const data = [
     {
         number: 1,
-        title: "",
+        title: "Daroo 3 Seater Sofa In Camel Beige",
+        Brand: "Febonic",
+        tab: "Furniture",
+        category:"Sofa",
+        subCategory: "3 Seater Sofas",
+        color: "Beige",
         image: "link",
-        price:"",
-        description:"",
-        ratings:"",
-        category:"",
-    }
+        price:"₹ 21,279 (56% off)",
+        description: ["Frame Material: Neem Wood","Secondary Material : Neem Wood Ply", "Leg Material : Neem Wood", "Fabric : Beso Velvet", "Foam Material: PU Foam", "Foam Density: 32 Kg", "Suspension System: Webbing Belt", "Cushion Material: Poly Fiber", "Seating Comfort - Mid Soft"],
+        ratings:"5",
+        Dimensions: "H 32 x W 73 x D 30 (In Inches)",
+        Weight: "55KG",
+    },
+    {
+        number: 2,
+        title: "Miranda 2 Seater Sofa In Navy Blue Colour",
+        Brand: "Woodsworth",
+        tab: "Furniture",
+        category:"Sofa",
+        subCategory: "2 Seater Sofas",
+        color: "Blue",
+        image: "link",
+        price:"₹ 15,279 (28% off)",
+        description: ["Frame- Pine Wood/9mm ply", "Upholstery-Polyester Fabric","Seat Foam- 32&26D","Back Cushion-Racron Polyester Fabric fiber","Seat- Elastic Belt-70mm & Zig zag spring","Legs-White ash wood with walnut polish.Throw cushion-2 nos."],
+        ratings:"5",
+        Dimensions: "H 35.5 x W 61 x D 35.5; Seating Height-18; Seating Depth - 22.5 (In Inches)",
+        Weight: "43KG",
+    },
+    {
+        number: 3,
+        title: "Guide Writing Table in Exotic Teak Finish",
+        Brand: "Globia creationsWoodsworth",
+        tab: "Furniture",
+        category:"Table",
+        subCategory: "Computer",
+        color: "Varnished",
+        image: "link",
+        price:"₹ 15,220 (28% off)",
+        description: ["Water resistant, fungus, termite & scratch proof","Premium grade hardware and fittings are used"],
+        ratings:"5",
+        Dimensions: "H 30 x W 59 x D 24 (In Inches)",
+        Weight: "60KG",
+    },
+    {
+        number: 4,
+        tab: "Furniture",
+        category: "Sofas",
+        subCategory: "1 Seater Sofa"
+    },
+    {
+        number: 5,
+        tab: "Furniture",
+        category: "Sofas",
+        subCategory: "Sofa Sets"
+    },
+    {
+        number: 6,
+        tab: "Furniture",
+        category: "Sofas",
+        subCategory: "LHS Section Sofas"
+    },
+    {
+        number: 7,
+        tab: "Furniture",
+        category: "Sofas",
+        subCategory: "RHS Section Sofas"
+    },
+    {
+        number: 8,
+        tab: "Furniture",
+        category: "Sofas",
+        subCategory: "Corner Sofas"
+    },
+    {
+        number: 9,
+        tab: "Furniture",
+        category: "Sofas",
+        subCategory: "Chaise Loungers"
+    },
+    {
+        number: 10,
+        tab: "Furniture",
+        category: "Sofas",
+        subCategory: "Sofa Cum Bed"
+    },
+    {
+        number: 11,
+        tab: "Furniture",
+        category: "Sofas",
+        subCategory: "Futons"
+    },
+    {
+        number: 12,
+        tab: "Furniture",
+        category: "Recliners",
+        subCategory: "1 Seater Recliners"
+    },
+    {
+        number: 13,
+        tab: "Furniture",
+        category: "Recliners",
+        subCategory: "2 Seater Recliners"
+    },
+    {
+        number: 14,
+        tab: "Furniture",
+        category: "Recliners",
+        subCategory: "3 Seater Recliners"
+    },
+    {
+        number: 15,
+        tab: "Furniture",
+        category: "Recliners",
+        subCategory: "Recliners Sets" 
+    },
+    {
+        number: 16,
+        tab: "Furniture",
+        category: "Sofa Chairs",
+        subCategory: "Wing Chairs"
+    },
+    {
+        number: 17,
+        tab: "Furniture",
+        category: "Sofa Chairs",
+        subCategory: "Lounge Chairs"
+    },
+    {
+        number: 18,
+        tab: "Furniture",
+        category: "Sofa Chairs",
+        subCategory: "Slipper Chairs"
+    },
+    {
+        number: 19,
+        tab: "Furniture",
+        category: "Sofa Chairs",
+        subCategory: "Barrel Chairs"
+    },
+    {
+        number: 20,
+        tab: "Furniture",
+        category: "Office Chairs",
+        subCategory: "Ergonomics Chairs"
+    },
+    {
+        number: 21,
+        tab: "Furniture",
+        category: "Office Chairs",
+        subCategory: "Executive Chairs"
+    },
+    {
+        number: 22,
+        tab: "Furniture",
+        category: "Office Chairs",
+        subCategory: "Training Chairs"
+    },
+    {
+        number: 23,
+        tab: "Furniture",
+        category: "Office Chairs",
+        subCategory: "Guest Chairs"
+    },
+    {
+        number: 24,
+        tab: "Furniture",
+        category: "Gaming Chairs",
+        subCategory: "Gaming Chairs"
+    },
+    {
+        number: 25,
+        tab: "Furniture",
+        category: "Bean Bags",
+        subCategory: "Bean Bags"
+    },
+    {
+        number: 26,
+        tab: "Furniture",
+        category: "Chairs",
+        subCategory: "Arm Chairs"
+    },
+    {
+        number: 27,
+        tab: "Furniture",
+        category: "Chairs",
+        subCategory: "Rocking Chairs"
+    },
+    {
+        number: 26,
+        tab: "Furniture",
+        category: "Chairs",
+        subCategory: "Arm Chairs"
+    },
 ]
 
 export default data
